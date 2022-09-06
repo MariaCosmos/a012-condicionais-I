72ab29174ba481f61f8ee47b53e28189a39f5561
MariaCosmos <cosmos.maria2003@gmail.com>
2022-09-06 15:49:26 -0300

    Exercicios concluídos - condicionais I

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 19
+let terminouMedio = true
+let naoCursandoFaculdade = false
 
-if(){
-
+if(idade >= 18){
+  console.log("é maior de idade!")
 }else{
-	
+	console.log("é menor de idade!")
+}
+
+if(terminouMedio){
+  console.log("terminou o ensino médio")
+}else{
+  console.log("não terminou o ensino médio")
+}
+
+if(naoCursandoFaculdade){
+  console.log("não está cursando faculdade!")
+}else{
+  console.log("está cursando faculdade!")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,9 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual é sua nacionalidade?").toLowerCase()
+
+if(nacionalidade === "brasileira" || nacionalidade === "argentina" || nacionalidade === "uruguaia" || nacionalidade === "chilena" || nacionalidade === "colombiana"){
+  console.log("sua nacionalidade é", nacionalidade)
+}else{
+  console.log("nacionalidade não encontrada")
+}
+
+
